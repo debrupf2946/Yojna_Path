9e9eaa9c397af3f044267892a92e36a48d6a40e2
debrupf2946 <f20212946@goa.bits-pilani.ac.in>
2025-01-24 10:29:59 +0530

    grock implimentation for whatsapp intigration

---
+++ experiments/whatsapp_intigration/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from twilio.twiml.messaging_response import MessagingResponse
+import sys
+import os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+from API_KEYS import GROQ_APIKEY
+from prompts import SYSTEM_PROMPT
+from groq import Groq
+
+
+
+
+app=FastAPI()
+
+gclient = Groq(api_key=GROQ_APIKEY)
+
+
+messages_history = [{"role": "system", "content": SYSTEM_PROMPT}]
+
+
+def generate_answer(question: str):
+    
+
+    
+    messages_history.append({"role": "user", "content": question})
+    
+    completion = gclient.chat.completions.create(
+                    model="mixtral-8x7b-32768",
+                    messages=messages_history,
+                    temperature=0.5,
+                    max_tokens=200,
+                    top_p=1,
+                    stream=False,
+                )
+                
+    assistant_response = completion.choices[0].message.content
+    print("Response from Groq client:", assistant_response)
+    messages_history.append({"role": "assistant", "content": assistant_response})
+    
+    return assistant_response
+
+
+
+question="suggest me any 2 govt schemes?"
+ans=generate_answer(question)
